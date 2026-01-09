32fa37e1dd50c8098aa4b703a0db5c768bf2fd4a
Gaurav <Shivalegaurav487@gmail.com>
2026-01-09 12:58:02 +0530

    Add files via upload

---
+++ Assignment01/SetAQ1.go
@@ -0,0 +1,8 @@
+package main
+  import "fmt"
+  func main(){
+	fmt.Println("Student name is Gaurav.")
+	fmt.Println("Roll number is 34.")
+	fmt.Println("Division is TYBCA.")
+	fmt.Println("College name is BJS.")
+}
